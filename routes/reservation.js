fd17b8f114b8427846ac83b5217d520597819c5b
Sola <solaocodes@gmai.com>
2019-07-18 20:17:01 -0400

    enable CORS on /reservation route.

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const cors = require('cors');
 
 const router = Router();
 
 /* GET reservations listing */
-router.get('/', (req, res) => {
+router.get('/', cors(), (req, res, next) => {
   res.json([
     {id: 1, name: "Chris, party of 2 @ 8pm"},
     {id: 2, name: "Edward, party of 5 @ 2pm"}
