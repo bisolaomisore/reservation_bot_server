6c0e02737955cebab4df83dbbfd9ed417edc2cbc
Sola <solaocodes@gmai.com>
2019-07-18 21:33:40 -0400

    Allow Twilio API to receive reservation requests
    - Receive and format reservation requests through sms
    - Respond w/ success or failure depending on opening
      and closing times of restaurant

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,29 +1,53 @@
-const { Router } = require('express');
-const cors = require('cors');
+const express = require('express');
+const router = express.Router();
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const router = Router();
+const reservationData = [];
 
-/* GET reservations listing */
-router.get('/', cors(), (req, res, next) => {
-  res.json([
-    {id: 1, name: "Chris, party of 2 @ 8pm"},
-    {id: 2, name: "Edward, party of 5 @ 2pm"}
-  ])
-});
+/* GET request */
+router.get('/', function(req, res, next) {
+  res.json(
+    reservationData
+  )
+})
 
-/* create reservations */
-router.post('/', (req, res, next) => {
-  var twiml = new MessagingResponse();
+/* POST create reservations */
+router.post('/', function(req, res, next) {
+  const twiml = new MessagingResponse();
 
-  // Content of text message to twilio API.
-  console.log(req.body.Body);
+  // The content of the text message sent to twilio.
+  let reservationMessage = req.body.Body.split('/');
+  let reservationDate = new Date(reservationMessage[2]);
+  let reservationHours = reservationDate.getHours();
+  let reservationMin =  (reservationDate.getMinutes() < 10) ? '0' + reservationDate.getMinutes() : reservationDate.getMinutes()
 
-  // Return message
-  twiml.message("We'll have your reservation details very soon!");
+  //  The message we send back after we make a reservation (successfully or
+  //  otherwise).
+  let message = '';
+
+  const restaurant = {
+    opening: 8,
+    closing: 22,
+    name: 'Hariet'
+  };
+
+  if (restaurant.opening < reservationHours && reservationHours < restaurant.closing - 1) {
+    message = 'Reservation successful';
+    const reservationId = reservationData.length + 1;
+    reservationData.push({
+      id: reservationId,
+      name: `${reservationMessage[0]}
+            ${reservationMessage[1]} at
+            ${reservationHours}:${reservationMin}`
+    });
+  } else {
+    message = 'Reservation unsucessful';
+  }
+
+  twiml.message(message);
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
-})
+});
 
 module.exports = router;
