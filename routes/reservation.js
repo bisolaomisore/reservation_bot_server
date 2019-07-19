4a20d28a221cef9e7ee20a6dfc4be083701c3ecd
Sola <solaocodes@gmai.com>
2019-07-19 13:05:54 -0400

    Integrate Slack API in to project
    - Configure reservation POST route to respond
      to reservations from Slack and Twilio API's

--- routes/reservation.js
+++ routes/reservation.js
@@ -20,48 +20,88 @@ router.get('/', cors(), (req, res, next) => {
 
 /* POST create reservations */
 router.post('/', (req, res, next) => {
-  const twiml = new MessagingResponse();
+  // Reservation message from Slack
+  if (req.body.command === '/text_reservation_bot') {
+    // The content of the text message sent to twilio.
+    let reservationMessage = req.body.text.split('/');
+    let firstName = reservationMessage[0];
+    let lastName = reservationMessage[1];
+    let datetime = reservationMessage[2];
+    let reservationDate = new Date(datetime);
+    let reservationHours = reservationDate.getHours();
+    let reservationMin =  (reservationDate.getMinutes() < 10) ? '0' + reservationDate.getMinutes() : reservationDate.getMinutes()
 
-  // The content of the text message sent to twilio.
-  let reservationMessage = req.body.Body.split('/');
-  let firstName = reservationMessage[0];
-  let lastName = reservationMessage[1];
-  let datetime = reservationMessage[2];
-  let reservationDate = new Date(datetime);
-  let reservationHours = reservationDate.getHours();
-  let reservationMin =  (reservationDate.getMinutes() < 10) ? '0' + reservationDate.getMinutes() : reservationDate.getMinutes()
+    //  The message we send back after we make a reservation (successfully or
+    //  otherwise).
+    var message = '';
 
-  //  The message we send back after we make a reservation (successfully or
-  //  otherwise).
-  var message = '';
+    const restaurant = {
+      opening: 8,
+      closing: 22,
+      name: 'Hariet'
+    };
 
-  const restaurant = {
-    opening: 8,
-    closing: 22,
-    name: 'Hariet'
-  };
+    if (restaurant.opening < reservationHours && reservationHours < restaurant.closing - 1) {
+      message = 'Reservation successful';
 
-  if (restaurant.opening < reservationHours && reservationHours < restaurant.closing - 1) {
-    message = 'Reservation successful';
+      // Add details of a successful reservation to the database.
+      let newReservation = {firstname: firstName, lastname: lastName, datetime: datetime};
+      Reservation.create(newReservation, (err, reservation) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("reservation successfully saved to database.");
+        }
+      });
+    } else {
+      message = 'Reservation unsucessful';
+    }
 
-    // Add details of a successful reservation to the database.
-    let newReservation = {firstname: firstName, lastname: lastName, datetime: datetime};
-    Reservation.create(newReservation, (err, reservation) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("reservation successfully saved to database.");
-      }
-    });
+    res.status(200).send(message); 
   } else {
-    message = 'Reservation unsucessful';
+    const twiml = new MessagingResponse();
+
+    // The content of the text message sent to twilio.
+    let reservationMessage = req.body.Body.split('/');
+    let firstName = reservationMessage[0];
+    let lastName = reservationMessage[1];
+    let datetime = reservationMessage[2];
+    let reservationDate = new Date(datetime);
+    let reservationHours = reservationDate.getHours();
+    let reservationMin =  (reservationDate.getMinutes() < 10) ? '0' + reservationDate.getMinutes() : reservationDate.getMinutes()
+
+    //  The message we send back after we make a reservation (successfully or
+    //  otherwise).
+    var message = '';
+
+    const restaurant = {
+      opening: 8,
+      closing: 22,
+      name: 'Hariet'
+    };
+
+    if (restaurant.opening < reservationHours && reservationHours < restaurant.closing - 1) {
+      message = 'Reservation successful';
+
+      // Add details of a successful reservation to the database.
+      let newReservation = {firstname: firstName, lastname: lastName, datetime: datetime};
+      Reservation.create(newReservation, (err, reservation) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("reservation successfully saved to database.");
+        }
+      });
+    } else {
+      message = 'Reservation unsucessful';
+    }
+
+    twiml.message(message);
+
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
   }
-
-  twiml.message(message);
-
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
 });
 
 const formatReservations = (data) => {
